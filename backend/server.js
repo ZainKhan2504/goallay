8f5ac0d7b058e918ba23e9990f707b2a0e25a50d
zaikh847 <zaikh847@student.liu.se>
2022-03-28 17:28:14 +0500

    Deploy settings

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -24,6 +25,19 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 // Error handle middleware
 app.use(errorHandler);
